e71099a828f4cd79d09b28beb39fd4d84aca8da4
Kyle Sessions <kage23@gmail.com>
2017-09-23 21:51:29 -0700

    Display improvements.

--- src/notifications/notificationsComponent.js
+++ src/notifications/notificationsComponent.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './notifications.css';
+
 class NotificationsComponent extends React.Component {
   static propTypes = {
     notifications: PropTypes.array,
