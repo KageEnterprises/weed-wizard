44828b933b4505ace1c030faef6437c97aeee4db
Kyle Sessions <kage23@gmail.com>
2018-09-26 17:57:53 -0700

    WIP I guess?

--- src/player/playerState.js
+++ src/player/playerState.js
@@ -2,6 +2,10 @@ import { getStrainById } from '../utils/weedUtils';
 
 const initialPlayerState = () => {
   const strainProps = getStrainById(0);
+  const strainProps1 = getStrainById(1);
+  const strainProps2 = getStrainById(2);
+  const strainProps3 = getStrainById(3);
+  const strainProps4 = getStrainById(4);
 
   return {
     highness: 0,
@@ -19,7 +23,35 @@ const initialPlayerState = () => {
         id: 0,
         quantity: 0.125, // in ozs
         selected: true,
-        seeds: 0
+        seeds: 4
+      },
+      {
+        ...strainProps1,
+        id: 1,
+        quantity: 0.125, // in ozs
+        selected: false,
+        seeds: 4
+      },
+      {
+        ...strainProps2,
+        id: 2,
+        quantity: 0.125, // in ozs
+        selected: false,
+        seeds: 4
+      },
+      {
+        ...strainProps3,
+        id: 3,
+        quantity: 0.125, // in ozs
+        selected: false,
+        seeds: 4
+      },
+      {
+        ...strainProps4,
+        id: 4,
+        quantity: 0.125, // in ozs
+        selected: false,
+        seeds: 4
       }
     ]
   };
